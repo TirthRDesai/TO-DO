936c7a99cd8bd0ac9287593eb18ee878200450d2
Tirth <tirthrdesai05@gmail.com>
2023-06-25 12:33:45 +0530

    location changed

--- TO-DO/index.js
+++ TO-DO/index.js
@@ -429,7 +429,9 @@ function logout_user(i, mytimer){
         });
     }
     setTimeout(function(){
-        window.location.assign("login.php");
+        var url = window.location.href;
+        url.replace("index.html", "login.php")
+        window.location.assign(url);
     },mytimer+1000);
 }
 
